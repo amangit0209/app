2c5616ebb82f69f9df7aa0a136a583026d8c4d9a
amangit0209 <amansingh601460@gmail.com>
2025-05-06 19:21:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title="BSE Scraper App Selector", page_icon="📈")
 st.title("📊 Welcome to BSE Scraper Dashboard")
 st.markdown("""
 Use the sidebar to navigate to one of the scraper tools:
-- **Quarterly Scraper** for quarterly financial data
-- **Annual Scraper** for yearly trends
+- **Quarterlyr** for quarterly financial data
+- **Annual** for yearly trends data
 """)
