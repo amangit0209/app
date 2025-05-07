d2cf0b6be923c565bfed2c95dbdf52932d04baec
amangit0209 <amansingh601460@gmail.com>
2025-05-07 17:48:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+
 
 # Default mappings and periods
 default_metric_mapping = {
